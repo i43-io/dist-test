1a5ad3cb059973703f59649067ee7588daa099b3
Tal <talrasha007@gmail.com>
2023-03-08 12:43:04 +0800

    draw chart.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,21 @@
 <script setup>
+import { ref } from 'vue'
+
 import DataInput from './components/DataInput.vue'
+import DataChart from './components/DataChart.vue'
+
+const data = ref([])
 </script>
 
 <template>
-  <div>
-    <data-input />
+  <div class="row">
+    <data-input v-model="data" />
+    <data-chart :data="data" />
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.row {
+  display: flex;
 }
 </style>
---
+++ src/components/DataChart.vue
@@ -0,0 +1,34 @@
+<script setup>
+import Highcharts from 'highcharts'
+import { ref, watch } from 'vue'
+
+const container = ref(null)
+
+const props = defineProps({ data: { type: Array } })
+watch(() => props.data, val => {
+  val = val.map(i => [parseFloat(i[0]), i[1]])
+  console.log(val)
+  const x = val.map(i => parseFloat(i[0]))
+  const y = val.map(i => i[1])
+  Highcharts.chart(container.value, {
+    title: {
+      text: ''
+    },
+    xAxis: {
+    },
+    series: [{ data: val }]
+  })
+})
+</script>
+
+<template>
+<div ref="container" class="chart"></div>
+</template>
+
+<style scoped scss>
+.chart {
+  margin-left: 10px;
+  height: 250px;
+  width: 640px;
+}
+</style>
