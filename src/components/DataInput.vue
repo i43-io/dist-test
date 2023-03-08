bf48c1d49fd38b0e12d17f647d2fd0f364eaf4fa
Tal <talrasha007@gmail.com>
2023-03-08 11:28:50 +0800

    data input.

---
+++ src/components/DataInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <textarea cols="16" rows="16"></textarea>
+    <button>查看</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+button {
+  display: block;
+  margin: 5px auto;
+  text-align: center;
+}
+</style>
